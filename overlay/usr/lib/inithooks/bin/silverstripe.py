0fbd9b680fe7a7a1378b8b6b063e8bbdca65521a
Alon Swartz <alon@turnkeylinux.org>
2012-06-13 16:24:28 +0300

    inithook to set admin email and password

---
+++ overlay/usr/lib/inithooks/bin/silverstripe.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+"""Set SilverStripe admin password and email
+
+Option:
+    --pass=     unless provided, will ask interactively
+    --email=    unless provided, will ask interactively
+
+"""
+
+import os
+import sys
+import getopt
+import hashlib
+
+from dialog_wrapper import Dialog
+from mysqlconf import MySQL
+
+def usage(s=None):
+    if s:
+        print >> sys.stderr, "Error:", s
+    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
+    print >> sys.stderr, __doc__
+    sys.exit(1)
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
+                                       ['help', 'pass=', 'email='])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    password = ""
+    email = ""
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt == '--pass':
+            password = val
+        elif opt == '--email':
+            email = val
+
+    if not password:
+        d = Dialog('TurnKey Linux - First boot configuration')
+        password = d.get_password(
+            "SilverStripe Password",
+            "Enter new password for the SilverStripe 'admin' account.")
+
+    if not email:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        email = d.get_email(
+            "SilverStripe Email",
+            "Enter email address for the SilverStripe 'admin' account.",
+            "admin@example.com")
+
+    salt = os.urandom(20).encode('hex')
+    hash = hashlib.sha1(password + salt).hexdigest()
+
+    m = MySQL()
+    m.execute('UPDATE silverstripe.Member SET Salt=\"%s\" WHERE ID=1;' % salt)
+    m.execute('UPDATE silverstripe.Member SET Password=\"%s\" WHERE ID=1;' % hash)
+    m.execute('UPDATE silverstripe.Member SET Email=\"%s\" WHERE ID=1;' % email)
+
+    m.execute('UPDATE silverstripe.MemberPassword SET Salt=\"%s\" WHERE ID=1;' % salt)
+    m.execute('UPDATE silverstripe.MemberPassword SET Password=\"%s\" WHERE ID=1;' % hash)
+
+
+if __name__ == "__main__":
+    main()
+
